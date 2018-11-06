e2c402401f0a61cd19eb7df22d44b56001c70f16
ageery <andrew_geery@windsorsolutions.com>
2018-11-06 14:23:23 -0500

    Set up local publishing.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,4 @@
+import org.gradle.jvm.tasks.Jar
 import org.jetbrains.dokka.DokkaConfiguration
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 import org.jetbrains.dokka.gradle.DokkaTask
@@ -7,11 +8,13 @@ val wicketVersion = "8.1.0"
 val junitVersion = "5.3.1"
 val servletApiVersion = "3.1.0"
 val kotlinxHtmlVersion = "0.6.10"
+val mavenPubName = "mavenJavaLibrary"
 
 plugins {
     kotlin("jvm") version "1.3.0"
     id("jacoco")
     id("org.jetbrains.dokka") version "0.9.16"
+    id("maven-publish")
 }
 
 //plugins.apply("org.jetbrains.dokka")
@@ -39,14 +42,6 @@ tasks.withType<Test> {
     }
 }
 
-tasks.withType<DokkaTask> {
-    outputFormat = "html"
-    externalDocumentationLink(delegateClosureOf<DokkaConfiguration.ExternalDocumentationLink.Builder> {
-        url = URL("https://ci.apache.org/projects/wicket/apidocs/8.x/")
-    })
-    cacheRoot = "default"
-}
-
 dependencies {
     compile("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compile("org.jetbrains.kotlin:kotlin-reflect")
@@ -63,4 +58,33 @@ dependencies {
     testRuntime("junit:junit:4.12")
 
     compile("org.jetbrains.kotlinx:kotlinx-html-jvm:$kotlinxHtmlVersion")
+}
+
+val dokkaJavadocTask = tasks.withType<DokkaTask> {
+    outputFormat = "html"
+    externalDocumentationLink(delegateClosureOf<DokkaConfiguration.ExternalDocumentationLink.Builder> {
+        url = URL("https://ci.apache.org/projects/wicket/apidocs/8.x/")
+    })
+    cacheRoot = "default"
+}
+
+val sourcesJar by tasks.creating(Jar::class) {
+    classifier = "sources"
+    from(sourceSets["main"].allSource)
+    setDuplicatesStrategy(DuplicatesStrategy.EXCLUDE)
+}
+
+val javadocJar by tasks.creating(Jar::class) {
+    dependsOn(dokkaJavadocTask)
+    classifier = "javadoc"
+}
+
+configure<PublishingExtension> {
+    publications {
+        create<MavenPublication>(mavenPubName) {
+            from(components.getByName("java"))
+            artifact(sourcesJar)
+            artifact(javadocJar)
+        }
+    }
 }
