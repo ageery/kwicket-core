ecb9223d7d1363d24577d33d4137a63a432a054c
ageery <andrew_geery@windsorsolutions.com>
2018-11-23 07:29:34 -0500

    More documentation of config classes/interfaces.

--- src/main/kotlin/org/kwicket/component/config/ZonedDateTimeFieldConfig.kt
+++ src/main/kotlin/org/kwicket/component/config/ZonedDateTimeFieldConfig.kt
@@ -11,13 +11,44 @@ import java.time.ZonedDateTime
 internal val IIZonedDateTimeFieldConfig<*>.requiresSubclass: Boolean
     get() = (this as IComponentConfig<*, *>).requiresSubclass || toZonedDate != null || toZonedTime != null || defaultTime != null
 
-interface IIZonedDateTimeFieldConfig<T: ZonedDateTime?> : IFormComponentConfig<ZonedDateTimeField, T> {
+/**
+ * Configuration for creating a [ZonedDateTimeField] component.
+ *
+ * @param T type of the component model
+ * @property toZonedDate lambda for converting a [ZonedDateTime] to a [LocalDate]
+ * @property toZonedTime lambda for converting a [ZonedDateTime] to a [LocalTime]
+ * @property defaultTime default time to use for a date-only value
+ */
+interface IIZonedDateTimeFieldConfig<T : ZonedDateTime?> : IFormComponentConfig<ZonedDateTimeField, T> {
     var toZonedDate: ((ZonedDateTime) -> LocalDate)?
     var toZonedTime: ((ZonedDateTime) -> LocalTime)?
     var defaultTime: (() -> LocalTime)?
 }
 
-class ZonedDateTimeFieldConfig<T: ZonedDateTime?>(
+/**
+ * Configuration for creating a [ZonedDateTimeField] component.
+ *
+ * @param T type of the model
+ * @property toZonedDate lambda for converting a [ZonedDateTime] to a [LocalDate]
+ * @property toZonedTime lambda for converting a [ZonedDateTime] to a [LocalTime]
+ * @property defaultTime default time to use for a date-only value
+ * @param model model for the component
+ * @param markupId optional unique id to use in the associated markup
+ * @param outputMarkupId whether to include an HTML id for the component in the markup
+ * @param outputMarkupPlaceholderTag whether to include a placeholder tag for the component in the markup when the
+ * component is not visible
+ * @param isVisible whether the component is initially visible
+ * @param isEnabled whether the component is initially enabled
+ * @param isVisibilityAllowed whether the component is allowed to be visible
+ * @param escapeModelStrings whether model strings should be escaped
+ * @param renderBodyOnly whether the tag associated with the component should be included in the markup
+ * @param behavior optional [Behavior] to add to the component
+ * @param behaviors optional List of [Behavior]s to add to the component
+ * @param stateless whether to include a hint that the component is stateless
+ * @param onConfig optional lambda to execute in the onConfigure lifecycle method
+ * @param postInit optional lambda to execute after the component has been created
+ */
+class ZonedDateTimeFieldConfig<T : ZonedDateTime?>(
     model: IModel<T>? = null,
     override var toZonedDate: ((ZonedDateTime) -> LocalDate)? = null,
     override var toZonedTime: ((ZonedDateTime) -> LocalTime)? = null,
