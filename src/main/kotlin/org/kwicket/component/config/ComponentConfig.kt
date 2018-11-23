fba3ffe3d0150d7cded2b1822e9571bb7233a6a6
ageery <andrew_geery@windsorsolutions.com>
2018-11-23 07:18:01 -0500

    Started documenting the config classes/interfaces.

--- src/main/kotlin/org/kwicket/component/config/ComponentConfig.kt
+++ src/main/kotlin/org/kwicket/component/config/ComponentConfig.kt
@@ -3,13 +3,36 @@ package org.kwicket.component.config
 import org.apache.wicket.Component
 import org.apache.wicket.behavior.Behavior
 import org.apache.wicket.model.IModel
-import java.io.Serializable
 
+/**
+ * Whether the [IComponentConfig] requires a subclass to create using standard Wicket components
+ */
 internal val IComponentConfig<*, *>.requiresSubclass: Boolean
     get() = onConfig != null || stateless != null
 
+/**
+ * Configuration for creating a Wicket [Component].
+ *
+ * @param C type of Wicket component
+ * @param T type of the component model
+ * @property model model for the component
+ * @property markupId optional unique id to use in the associated markup
+ * @property outputMarkupId whether to include an HTML id for the component in the markup
+ * @property outputMarkupPlaceholderTag whether to include a placeholder tag for the component in the markup when the
+ * component is not visible
+ * @property isVisible whether the component is initially visible
+ * @property isEnabled whether the component is initially enabled
+ * @property isVisibilityAllowed whether the component is allowed to be visible
+ * @property escapeModelStrings whether model strings should be escaped
+ * @property renderBodyOnly whether the tag associated with the component should be included in the markup
+ * @property behavior optional [Behavior] to add to the component
+ * @property behaviors optional List of [Behavior]s to add to the component
+ * @property stateless whether to include a hint that the component is stateless
+ * @property onConfig optional lambda to execute in the onConfigure lifecycle method
+ * @property postInit optional lambda to execute after the component has been created
+ */
 interface IComponentConfig<C : Component, T> {
-    val model: IModel<T>?
+    var model: IModel<T>?
     var markupId: String?
     var outputMarkupId: Boolean?
     var outputMarkupPlaceholderTag: Boolean?
@@ -23,14 +46,29 @@ interface IComponentConfig<C : Component, T> {
     var stateless: Boolean?
     var onConfig: (C.() -> Unit)?
     var postInit: (C.() -> Unit)?
-//    val requiresSubclass: Boolean
-//        get() = onConfig != null || stateless != null
-//    val isValid: Boolean
-//        get() = true
 }
 
+/**
+ * Configuration for creating a Wicket [Component].
+ *
+ * @property model model for the component
+ * @property markupId optional unique id to use in the associated markup
+ * @property outputMarkupId whether to include an HTML id for the component in the markup
+ * @property outputMarkupPlaceholderTag whether to include a placeholder tag for the component in the markup when the
+ * component is not isVisible
+ * @property isVisible whether the component is initially visible
+ * @property isEnabled whether the component is initially enabled
+ * @property isVisibilityAllowed whether the component is allowed to be visible
+ * @property escapeModelStrings whether model strings should be escaped
+ * @property renderBodyOnly whether the tag associated with the component should be included in the markup
+ * @property behavior optional [Behavior] to add to the component
+ * @property behaviors optional List of [Behavior]s to add to the component
+ * @property stateless whether to include a hint that the component is stateless
+ * @property onConfig optional lambda to execute in the onConfigure lifecycle method
+ * @property postInit optional lambda to execute after the component has been created
+ */
 open class ComponentConfig<C : Component, T>(
-    override val model: IModel<T>? = null,
+    override var model: IModel<T>? = null,
     override var markupId: String? = null,
     override var outputMarkupId: Boolean? = null,
     override var outputMarkupPlaceholderTag: Boolean? = null,
